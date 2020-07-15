271bd98e39dbc5bad2a3448e2f5cc5fb5e3f135d
Estela de Oliveira <65990523+ste2021@users.noreply.github.com>
2020-07-15 14:06:54 -0300

    Update APP orientacoes.js

--- APP orientacoes.js
+++ APP orientacoes.js
@@ -1,12 +1,12 @@
 let alunoA = "Lucas";
 let alunoB = "Joao";
 
-let nomeAlunos = ["Lucas", "Joao", "Pedro", "Gustavo"];
+let nomeAlunos = ["Lucas", "Joao", "Pedro", "Gustavo"];//COLCHETES SERVEM PARA FAZER ARRAYS
 
-console.log(nomeAlunos);
+//console.log(nomeAlunos []); //para selecionar algum nome do array, você precisa colocar entre colchetes e escolher o nº do array,(lembrando q sempre começa pelo 0
 
 //i++ = é a mesma coisa q i+1, 
-for (let i= 0; i < 4; i++){
+for (let i= 0; i < 4; i++){                            //precisa declarar o ponto de partida
     console.log("Nome do Aluno: " + nomeAlunos [i]);
 }
 
